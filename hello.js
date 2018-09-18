0f314a09dca0364e560fefb7331f76afdd85245f
siva <sivasai.sarapu@hedgehoglab.com>
2018-09-18 15:59:46 +0530

    sdf

--- hello.js
+++ hello.js
@@ -1,6 +1,13 @@
+<<<<<<< Updated upstream
 console.log("hello world")
 const hello  ()=>`hello`
 console.log("this is feature branch branch")
 console.log("commiting in feature")
 console.log("this is feature branch branch")
 console.log("commiting in feature")
+=======
+function hello() {
+    return "hello";
+}
+console.log("hello world")
+>>>>>>> Stashed changes
