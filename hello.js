1a9610917791b76b15b10aeaf79acddf5e739cbd
siva <sivasai.sarapu@hedgehoglab.com>
2018-09-18 16:22:55 +0530

    commit feature branch

--- hello.js
+++ hello.js
@@ -6,7 +6,10 @@ console.log("this  branch")
 console.log("commiting in master")
 console.log("hello my name is sai and trying to understand git")
 console.log("this is master branch")
-
+const mom = ()=>{
+    let x = 10;
+    return x+50;
+}
 console.log("this is master branch")
 console.log("commiting in master")console.log("hello my name is sai and trying to understand git")
 console.log("this is master branch")
