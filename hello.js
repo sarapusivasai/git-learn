25e0cece3e4ae6ab7434a7eea237d595055f0094
siva <sivasai.sarapu@hedgehoglab.com>
2018-09-18 16:19:41 +0530

    fxv

--- hello.js
+++ hello.js
@@ -6,7 +6,10 @@ console.log("this  branch")
 console.log("commiting in master")
 console.log("hello my name is sai and trying to understand git")
 console.log("this is master branch")
-
+const hello = ()=>{
+    let x = 10;
+    return x*x;
+}
 console.log("this is master branch")
 console.log("commiting in master")console.log("hello my name is sai and trying to understand git")
 console.log("this is master branch")
