b8a3be3346fb8eb77bff66dbbbbb5592844ca9d7
siva <sivasai.sarapu@hedgehoglab.com>
2018-09-18 13:13:49 +0530

    commiting hello.js stage file in feature

--- hello.js
+++ hello.js
@@ -1,2 +1,3 @@
 console.log("hello this is feature branch")
 console.log("currently i am in feature branch made some changes in hello.js and stashing them")
+console.log("applying stash againg and going to master")
