b5147e8f9fa2d6a2004bd8ae8a3b0e85b0662d22
siva <sivasai.sarapu@hedgehoglab.com>
2018-09-18 16:28:58 +0530

    sdf

--- hello.js
+++ hello.js
@@ -1,7 +1,15 @@
 console.log("hello my name isg to understand git")
 console.log("this is master branch")
+const mom = ()=>{
+    let x = 10;
+    return x+50;
+}
 console.log("commiting in master")
 console.log("hello my name is sai and trying to understand git")
+const mom = ()=>{
+    let x = 10;
+    return x+50;
+}
 console.log("this  branch")
 console.log("commiting in master")
 console.log("hello my name is sai and trying to understand git")
@@ -15,5 +23,9 @@ console.log("commiting in master")console.log("hello my name is sai and trying t
 console.log("this is master branch")
 console.log("commiting in master")
 console.log("hello my namederstand git")
+const mom = ()=>{
+    let x = 10;
+    return x+50;
+}
 console.log("this is master branch")
 console.log("commi")
