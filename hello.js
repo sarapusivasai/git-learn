b923d0216bcc6569b136421c6733412077e9ca1d
siva <sivasai.sarapu@hedgehoglab.com>
2018-09-18 13:27:46 +0530

    commiting feature

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
 console.log("hello my name is sai and trying to understand git")
-console.log("this is master branch")
-console.log("commiting in master")
+console.log("this is feature branch branch")
+console.log("commiting in feature")
