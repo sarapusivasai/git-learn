886185bf3191483bfa61eea9c0224947841ac4dc
siva <sivasai.sarapu@hedgehoglab.com>
2018-09-19 13:43:29 +0530

    adding sms

--- hello.js
+++ hello.js
@@ -5,7 +5,7 @@ const hello = ()=>{
     return x*x;
 }
 console.log("commiting in master")
-console.log("hello my name is sai and trying to understand git")
+console.log("hellosff my name is sai and trying to understand git")
 const hello = ()=>{
     let x = 10;
     return x*x;
@@ -13,7 +13,7 @@ const hello = ()=>{
 console.log("this  branch")
 console.log("commiting in master")
 console.log("hello my name is saisdf and trying to understand git")
-console.log("this is master branch")
+console.log("this isfsdfs master branch")
 const hello = ()=>{
     let x = 10;
     return x*x;
