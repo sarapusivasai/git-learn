359c943294373d9df0d257586372d87d6df58f53
siva <sivasai.sarapu@hedgehoglab.com>
2018-09-19 17:18:22 +0530

    dvsvefv

--- hello.js
+++ hello.js
@@ -1,56 +1,15 @@
 
-console.log("shiva sai to understand git")
-console.log("this is masgsfd dfbdfter branch")
-console.log("hello my name is sai and trying to understand git")
-console.log("this is an hell branch")
-const hello = ()=>{
-    let x = 10;
-    return x*x;
-}
-console.log("hello my name isg to understand git")
-console.log("this is master branch")
-const mom = ()=>{
-    let x = 10;
-    return x+50;
-}
-console.log("commiting in master")
-console.log("hello my name is sai and trying to understand git")
-const mom = ()=>{
-    let x = 10;
-    return x+50;
-const hello = ()=>{
-    let x = 10;
-    return x*x;
-}
 console.log("commivting in master")
 console.log("fsdsv my name is sai and vasdfsdftrying to understand git")
-const hello = ()=>{
-    let x = 10;
-    return x*x;
-}
 console.log("this  branch")
 console.log("commiting in master")
 console.log("hello my name is saisdf and trying to understand git")
 console.log("this isfsdfssfsf master branch")
-const hello = ()=>{
 console.log("hello my name is sai and trying to understand git")
 console.log("this is master branch")
-const mom = ()=>{
-    let x = 10;
-    return x+50;
-}
 console.log("this is master branch")
 console.log("commiting in master")
 console.log("hello my name is sai and trying to understand git")
 console.log("this is master branch")
 console.log("commiting in master")
 console.log("hello my namederstand git")
-const mom = ()=>{
-    let x = 10;
-    return x+50;
-const hello = ()=>{
-    let x = 10;
-    return x*x;
-}
-console.log("this is master branch")
-console.log("commi");
